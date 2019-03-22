d13f183aeb6557ed8258e2aaa5036ea3287b9f20
jkl1999 <99kylel@gmail.com>
2019-03-22 16:07:42 -0400

    100 song max
    Contains fix to limit the number of songs in the playlist to 100.

--- indieheads.py
+++ indieheads.py
@@ -54,10 +54,25 @@ if token:
             continue
        
         for s in temp['tracks']['items']:
+            if(len(trackLinks)<100):
+                trackLinks.append(s['id'])
 
-             trackLinks.append(s['id'])
-
+             
     existing_tracks = sp.user_playlist_tracks(token,'75svY6VFRSQ1CCXZa6t9Bk')
+
+    #keeps playlist to a max 100 songs
+    if((existing_tracks['total']+len(trackLinks))>100):
+        dTracks = []
+        
+        for x in range((existing_tracks['total']+len(trackLinks))-100):
+            try:
+
+                dtemp = existing_tracks['items'][x]
+                dTracks.append(dtemp['track']['id'])
+            except IndexError:
+                continue
+       
+        sp.user_playlist_remove_all_occurrences_of_tracks(username, '75svY6VFRSQ1CCXZa6t9Bk', dTracks, snapshot_id=None)
 
     uriList = []
 
